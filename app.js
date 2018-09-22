f63f936eddf562e39e0740fd0d99031b6ebed3c5
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-09-22 16:30:23 -0400

    added modal to display houses to choose from and made it so when one is clicked, it is pushed to an array

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(()=> {
+  // call the function that will prompt the user to pick a house
+  selectHouse();
+  // call the function that will make the sorting hat appear and say who goes first
+
   // call function that will create the game board
   createBoard();
 
--- connect4.js
+++ connect4.js
@@ -1,15 +1,47 @@
 //==============================================================//
-//============== JAVASCRIPT VARIABLES & FUNCTIONS ==============//
+//              JAVASCRIPT VARIABLES & FUNCTIONS                //
 //==============================================================//
 
 // define the function that will populate the grid
 const createBoard = () => {
+  // declare a variable that will represent the game board, set it to the class for the game board using jQuery
   const $board = $('.connectFour');
   // use a for loop to generate the divs that will create the grid
   for (let i = 0; i < 42; i++) {
+    // declare a variable and set it to a div tag using jQuery
     const $circle = $('<div>');
-    // add a class to style the circles
+    // add a class to set a width and height for the circles
     $circle.addClass('circle');
+    // append the circles to the game board
     $board.append($circle);
   }
+};
+
+// declare variables for the selectHouse modal
+const $gryffindor = $('#gryffindor');
+const $ravenclaw = $('#ravenclaw');
+const $hufflepuff = $('#hufflepuff');
+const $slytherin = $('#slytherin');
+// make array consisting of two players for sorting hat to loop through
+const $players = [];
+
+// define a function that will prompt the user to pick a house (use a modal)
+const selectHouse = () => {
+  $('#selectHouse').show(2000);
+  $('#selectHouseBox > button').on('click', (event) => {
+    let house = $(event.currentTarget).children();
+    $(house).css('opacity', '0.2');
+    $players.push(house);
+    console.log($players);
+  })
 }
+
+const closeModal = () => {
+  $('#selectHouse').hide('slow');
+}
+
+// write if else statements that will check for which house was selected
+  // once player one has selected, make that house unclickable
+  // have the modal close upon selection from player two
+
+//
