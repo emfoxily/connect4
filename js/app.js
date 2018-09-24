0a7c6fe0da072a031fbec667d8ece32b8dd023ee
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-09-24 16:19:27 -0400

    redid board by using jquery to build it again as well as made it so the circles clicked are saved to a 2d array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(()=> {
   // call the function that will prompt the user to pick a house
   selectHouse();
+  createBoard();
 })
--- js/connect4.js
+++ js/connect4.js
@@ -2,6 +2,28 @@
 //              JAVASCRIPT VARIABLES & FUNCTIONS                //
 //==============================================================//
 
+let board = [];
+
+const createBoard = () => {
+  const $board = $('#connectFour');
+  for (let row = 0; row < 6; row++) {
+    let boardRow = [];
+    const $row = $('<div>')
+      .addClass('row');
+    for (let circle = 0; circle < 7; circle++) {
+      const $circle = $('<div>')
+      .addClass('circle')
+      .one('click', move);
+    boardRow.push($circle);
+    // const house = null;
+    // $circle.data('house', house)
+    $row.append($circle);
+    }
+    board.push(boardRow);
+    $board.append($row);
+  }
+}
+
 // make an empty array to push the selected houses to
   const $players = [];
 // declare variables for the players, but don't assign values
@@ -12,7 +34,7 @@
 const selectHouse = () => {
   $('#selectHouse').show(2000);
   // create event for when crest is clicked
-  $('#selectHouseBox > div').on('click', (event) => {
+  $('#selectHouseBox > div').one('click', (event) => {
     // give crest low opacity upon being clicked as if to say the house cannot be chosen again
     $(event.currentTarget).css('opacity', '0.2');
     // push the crest image to the players array
@@ -58,15 +80,23 @@ const closeModal = () => {
 // }
 
 // make an array of moves that goes to the number of circles
-  const moves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+  const moves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                  19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                  34, 35, 36, 37, 38, 39, 40, 41, 42];
+
+// make an empty array for board selections to live
+
+// calculate winner using the array
+
 
 
   const move = () => {
-    if (moves[0] % 2 === 0) {
+    if (moves[0] % 2 !== 0) {
     $(event.currentTarget).addClass($playerOne);
     moves.shift();
-  } else if (moves[0] % 2 !== 0) {
+  } else if (moves[0] % 2 === 0) {
     $(event.currentTarget).addClass($playerTwo);
     moves.shift();
     }
+    console.log(grid);
   }
